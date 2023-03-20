d9450007b316d9fce8d05111ce1c950c1eb1a140
Jonathan <joohny_2014@hotmail.com>
2023-03-20 10:41:04 -0300

    Navbar & css

---
+++ moveiseliane/pages/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image"
+import styles from '../../../styles/Navbar.module.css'
+export default function Navbar() {
+    return(
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Image src="/images/moveiseliane.jpg" width="30" height="30" alt="Eliane"/>
+                <h1>Moveis Eliane</h1>
+            </div>
+            <ul className={styles.link_items}>
+                <li>
+                    <Link href="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link href="/moveis">Moveis</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}

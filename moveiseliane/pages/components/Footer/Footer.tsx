d1c6b6ac60e89e79bc438e885bc645df604775c5
Jonathan <joohny_2014@hotmail.com>
2023-03-20 10:41:23 -0300

    Footer & css

---
+++ moveiseliane/pages/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from "../../../styles/Footer.module.css"
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <p>
+                <span>Moveis Eliane</span>
+                &copy; 2004
+            </p>
+        </footer>
+    )
+}
